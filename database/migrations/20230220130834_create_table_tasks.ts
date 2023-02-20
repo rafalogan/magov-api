e7d81d392786fe4986545254f98fd61d486a4faa
RafaDraw <www.rafadraw@gmail.com>
2023-02-20 10:37:59 -0300

    create tasks migration

---
+++ database/migrations/20230220130834_create_table_tasks.ts
@@ -0,0 +1,23 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable('tasks', (table: Knex.TableBuilder) => {
+		table.increments('id').primary();
+		table.string('title', 155).notNullable();
+		table.binary('description').nullable();
+		table.integer('cost').nullable();
+		table.timestamp('start').notNullable();
+		table.timestamp('end').notNullable();
+		table.integer('level').notNullable();
+		table.integer('status').notNullable();
+		table.integer('proposition_id').unsigned().references('id').inTable('propositions').nullable();
+		table.integer('demand_id').unsigned().references('id').inTable('demands').nullable();
+		table.integer('unit_id').unsigned().references('id').inTable('units').notNullable();
+		table.integer('plaintiff_id').unsigned().references('id').inTable('plaintiffs').notNullable();
+		table.integer('tenacy_id').unsigned().references('id').inTable('tenacies').notNullable();
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTable('tasks');
+}
