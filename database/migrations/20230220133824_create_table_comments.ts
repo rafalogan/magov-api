b0b2fc52a1fd6edc318cae109d431f95f50ef907
RafaDraw <www.rafadraw@gmail.com>
2023-02-20 10:45:21 -0300

    create comments migration

---
+++ database/migrations/20230220133824_create_table_comments.ts
@@ -0,0 +1,16 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable('comments', (table: Knex.TableBuilder) => {
+		table.integer('id').primary();
+		table.binary('comment').notNullable();
+		table.boolean('active').notNullable().defaultTo(true);
+		table.integer('task_id').unsigned().references('id').inTable('tasks').nullable();
+		table.integer('user_id').unsigned().references('id').inTable('users').notNullable();
+		table.integer('tenancy_id').unsigned().references('id').inTable('tenancies').notNullable();
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTable('comments');
+}
