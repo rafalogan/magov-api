fa0cf1974f745aa891c47cc3dd44981fb402b2b8
RafaDraw <www.rafadraw@gmail.com>
2023-02-24 19:24:31 -0300

    fix tablebuilder in migration

--- database/migrations/20230216194615_create_table_tenancies_plans.ts
+++ database/migrations/20230216194615_create_table_tenancies_plans.ts
@@ -1,7 +1,7 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-	return knex.schema.createTable('tenancies_plans', (table: Knex.Tablebuilder) => {
+	return knex.schema.createTable('tenancies_plans', (table: Knex.TableBuilder) => {
 		table.integer('tenancy_id').unsigned().references('id').inTable('tenancies').notNullable().primary();
 		table.integer('plan_id').unsigned().references('id').inTable('plans').notNullable().primary();
 		table.primary(['tenancy_id', 'plan_id']);
