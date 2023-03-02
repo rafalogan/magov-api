7feca2cc53a35998295c8e4a8bff1ee2a77a79de
RafaDraw <www.rafadraw@gmail.com>
2023-03-02 16:52:17 -0300

    refactor Not Found

--- src/core/routes/notfound.route.ts
+++ src/core/routes/notfound.route.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express';
 
-export const notfoundRoute = (req: Request, res: Response, next: any) => {
+export const notfoundRoute = (req: Request, res: Response) => {
 	res.status(404).send({
 		status: 404,
 		message: 'Not Found Route',
 	});
 };
 
-export const methodNotAllowed = (req: Request, res: Response, next?: any) => {
+export const methodNotAllowed = (req: Request, res: Response) => {
 	res.status(405).send({
 		status: 405,
 		message: 'Method Not Allowed',
