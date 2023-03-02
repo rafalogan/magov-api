044bd06c65fa44f7e71bc933cf83900229b8b0a4
RafaDraw <www.rafadraw@gmail.com>
2023-03-02 15:54:33 -0300

    implement UnitModule

--- src/modules/unit/unit.controller.ts
+++ src/modules/unit/unit.controller.ts
@@ -2,10 +2,11 @@ import { BAD_REQUEST } from 'http-status';
 import { Request, Response } from 'express';
 
 import { Controller } from 'src/core/controllers';
-import { requiredFields, setAddress, notExistisOrError, isRequired, existsOrError } from 'src/utils';
+import { existsOrError, isRequired, notExistisOrError, requiredFields, setAddress } from 'src/utils';
 import { UnitService } from 'src/services';
 import { getTenancyByToken, ResponseHandle } from 'src/core/handlers';
-import { UnitModel } from 'src/repositories/models';
+import { ReadOptionsModel, UnitModel } from 'src/repositories/models';
+import { IReadOptions } from 'src/repositories/types';
 
 export class UnitController extends Controller {
 	constructor(private unitService: UnitService) {
@@ -47,9 +48,23 @@ export class UnitController extends Controller {
 		} catch (message: any) {
 			return ResponseHandle.onError({ res, message, status: BAD_REQUEST });
 		}
+
+		const options = new ReadOptionsModel({ ...req.query, tenancyId } as IReadOptions);
+
+		this.unitService
+			.read(options, Number(id))
+			.then(data => ResponseHandle.onSuccess({ res, data, status: data.status }))
+			.catch(err => ResponseHandle.onError({ res, message: err.message, status: err.status }));
 	}
 
-	remove(req: Request, res: Response) {}
+	remove(req: Request, res: Response) {
+		const { id } = req.params;
+
+		this.unitService
+			.desactve(Number(id))
+			.then((data: any) => ResponseHandle.onSuccess({ res, data, status: data.status }))
+			.catch(err => ResponseHandle.onError({ res, err, message: err.message, status: err.status }));
+	}
 
 	private isValidRequest(req: Request) {
 		const { name, cnpj, phone } = req.body;
