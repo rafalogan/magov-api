e8459541e867c7784eae4eadbdfac8631de5d5d1
RafaDraw <www.rafadraw@gmail.com>
2023-03-04 20:32:07 -0300

    fix migration type error

--- database/migrations/20230304144715_create_table_sales.ts
+++ database/migrations/20230304144715_create_table_sales.ts
@@ -2,12 +2,11 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
 	return knex.schema.createTable('sales', (table: Knex.TableBuilder) => {
-		table.integer('seller_id').unsigned().references('id').inTable('users').notNullable();
+		table.integer('saller_id').unsigned().references('id').inTable('users').notNullable();
 		table.integer('tenancy_id').unsigned().references('id').inTable('tenancies').notNullable();
-		table.integer('user_id').unsigned().references('id').inTable('users').notNullable();
+		table.primary(['saller_id', 'tenancy_id']);
 		table.timestamp('commision_date').notNullable();
 		table.integer('commission_value').notNullable();
-		table.primary(['saller_id', 'tenancy_id', 'user_id']);
 	});
 }
 
