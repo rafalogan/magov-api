81eb9a4e70dbd4cc076590b75ebf3e7f93d2d4a7
RafaDraw <www.rafadraw@gmail.com>
2023-03-10 18:20:53 -0300

    Implement PlaintiffModule

--- src/services/plaintiff.service.ts
+++ src/services/plaintiff.service.ts
@@ -1,4 +1,5 @@
 import { FORBIDDEN, INTERNAL_SERVER_ERROR, NOT_FOUND } from 'http-status';
+import { onLog } from 'src/core/handlers';
 import { Plaintiff } from 'src/repositories/entities';
 
 import { PaginationModel, PlaintiffModel, ReadOptionsModel } from 'src/repositories/models';
@@ -13,17 +14,20 @@ export class PlaintiffService extends DatabaseService {
 
 	async create(data: PlaintiffModel) {
 		try {
-			const fromBD = await this.getPlaintiff(data.cpfCnpj, data.tenancyId as number);
+			const fromBD = await this.getPlaintiff(`${data.cpfCnpj}`, data.tenancyId as number);
 
-			notExistisOrError(fromBD.id, { message: 'Plaintiff already exists', status: FORBIDDEN });
+			notExistisOrError(fromBD?.id, { message: 'Plaintiff already exists', status: FORBIDDEN });
+
 			const toSave = new Plaintiff({ ...data } as IPlantiff);
 			const [id] = await this.db('plaintiffs').insert(convertDataValues(toSave));
 			existsOrError(Number(id), { message: 'Internal server error', err: id, status: INTERNAL_SERVER_ERROR });
 
-			const [addressId] = await this.db('adresses').insert(convertDataValues(data.address));
+			const [addressId] = await this.db('adresses').insert(convertDataValues({ ...data.address, plaintiffId: id }));
 			existsOrError(Number(addressId), { message: 'Internal server error', err: addressId, status: INTERNAL_SERVER_ERROR });
 
-			const [contactId] = await this.db('contacts').insert(convertDataValues({ email: data.email, phone: data.phone }));
+			const [contactId] = await this.db('contacts').insert(
+				convertDataValues({ email: data.email, phone: data.phone, plaintiffId: id, tenancyId: data.tenancyId })
+			);
 			existsOrError(Number(contactId), { message: 'Internal server error', err: contactId, status: INTERNAL_SERVER_ERROR });
 
 			return { message: 'Plantiff saved successfully', data: { ...data, id, address: { ...data.address, id: addressId }, contactId } };
@@ -41,7 +45,7 @@ export class PlaintiffService extends DatabaseService {
 
 			const toUpdate = new Plaintiff({ ...fromBD, ...data, tenancyId: fromBD.tenancyId });
 			await this.db('plaintiffs').where({ id }).andWhere({ tenancy_id: data.tenancyId }).update(convertDataValues(toUpdate));
-			await this.db('address')
+			await this.db('adresses')
 				.where({ plaintiff_id: fromBD.id })
 				.update(convertDataValues({ ...fromBD.address, ...data.address }));
 			await this.db('contacts')
@@ -56,24 +60,19 @@ export class PlaintiffService extends DatabaseService {
 
 	async read(options: ReadOptionsModel, id?: number) {
 		try {
-			const { page, limit, order, orderBy, tenancyId } = options;
+			const { tenancyId } = options;
 			existsOrError(tenancyId, { message: isRequired('Query param tenancyId'), status: FORBIDDEN });
 
 			if (id) return this.getPlaintiff(id, options.tenancyId as number);
 
-			const total = await this.getCount('plaintiffs', tenancyId);
-			const pagination = new PaginationModel({ page, limit, total });
-
 			return this.db('plaintiffs')
-				.select('id', 'name', 'cpf_cnpj', 'institute', 'institute_type_id')
+				.select('id', 'name', 'cpf_cnpj', 'institute', 'institute_type_id', 'tenancy_id', 'active')
 				.where({ tenancy_id: tenancyId })
-				.limit(limit)
-				.offset(page * limit - limit)
-				.orderBy(orderBy || 'id', order || 'asc')
 				.then(res => {
 					existsOrError(Array.isArray(res), { message: 'Internal error', status: INTERNAL_SERVER_ERROR });
-					const data = res.map(i => convertDataValues({ ...i, id: Number(i.id), institute_type_id: Number(i.institute_type_id) }, 'camel'));
-					return { data, pagination };
+					return res.map(i =>
+						convertDataValues({ ...i, id: Number(i.id), institute_type_id: Number(i.institute_type_id), active: !!i.active }, 'camel')
+					);
 				})
 				.catch(err => err);
 		} catch (err) {
@@ -83,39 +82,44 @@ export class PlaintiffService extends DatabaseService {
 
 	async getPlaintiff(value: number | string, tenancyId: number) {
 		try {
+			onLog('find value', value);
+			onLog('tenantId', tenancyId);
+
+			const plaintiffFields = {
+				id: 'p.id',
+				name: 'p.name',
+				birthday: 'p.birthday',
+				cpf_cnpj: 'p.cpf_cnpj',
+				institute: 'p.institute',
+				relationship_type: 'p.relationship_type',
+				observation: 'p.observation',
+				relatives: 'p.relatives',
+				voter_registration: 'p.voter_registration',
+				parent_id: 'p.parent_id',
+				institute_type_id: 'p.institute_type_id',
+				tenancy_id: 'p.tenancy_id',
+			};
+			const contactFields = { email: 'c.email', phone: 'c.phone' };
+			const addressFields = {
+				cep: 'a.cep',
+				street: 'a.street',
+				number: 'a.number',
+				complement: 'a.complement',
+				district: 'a.district',
+				city: 'a.city',
+				uf: 'a.uf',
+			};
+
 			const fromBD = await this.db({ p: 'plaintiffs', c: 'contacts', a: 'adresses' })
-				.select(
-					{
-						id: 'p.id',
-						name: 'p.name',
-						birthday: 'p.birthday',
-						cpf_cnpj: 'p.cpf_cnpj',
-						institute: 'p.institute',
-						relationship_type: 'p.relationship_type',
-						observation: 'p.observation',
-						relatives: 'p.relatives',
-						voter_registration: 'p.voter_registration',
-						parent_id: 'p.parent_id',
-						institute_type_id: 'p.institute_type_id',
-						tenancy_id: 'p.tenancy_id',
-					},
-					{ email: 'c.email', phone: 'c.phone' },
-					{
-						cep: 'a.cep',
-						street: 'a.street',
-						number: 'a.number',
-						complement: 'a.complement',
-						district: 'a.district',
-						city: 'a.city',
-						uf: 'a.uf',
-					}
-				)
-				.where('p.id', value)
-				.orWhere('p.cpf_cnpj', value)
-				.andWhere('p.tenancy_id', tenancyId)
+				.select(plaintiffFields, contactFields, addressFields)
+				.where('p.tenancy_id', tenancyId)
 				.andWhere('c.plaintiff_id', 'p.id')
 				.andWhere('a.plaintiff_id', 'p.id')
+				.orWhere('p.id', value)
+				.orWhere('p.cpf_cnpj', value)
 				.first();
+
+			onLog('find DB', fromBD);
 
 			existsOrError(fromBD?.id, { message: 'Plaintiffs not found', status: NOT_FOUND });
 
