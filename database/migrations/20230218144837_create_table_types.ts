457a3640f4037594e39f465397877e98492d3ea0
RafaDraw <www.rafadraw@gmail.com>
2023-05-02 16:02:56 -0300

    add types list

--- database/migrations/20230218144837_create_table_types.ts
+++ database/migrations/20230218144837_create_table_types.ts
@@ -3,9 +3,10 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
 	return knex.schema.createTable('types', (table: Knex.TableBuilder) => {
 		table.increments('id').primary();
-		table.string('name', 155).notNullable().unique();
+		table.string('name', 155).notNullable();
 		table.boolean('active').notNullable().defaultTo(true);
 		table.binary('description').nullable();
+		table.integer('parent_id').unsigned().references('id').inTable('types').nullable();
 	});
 }
 
