4b3736a426a176d95dec790f4c588012a764d482
RafaDraw <712020:ad0a1fe8-71f7-4d68-bc04-c1239416cf45@bots.bitbucket.org>
2023-06-22 17:37:56 -0300

    fix field of migation

--- database/migrations/20230220144741_create_table_government_expenses_payments.ts
+++ database/migrations/20230220144741_create_table_government_expenses_payments.ts
@@ -2,7 +2,7 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
 	return knex.schema.createTable('government_expenses_payment', (table: Knex.TableBuilder) => {
-		table.integer('government_expanse_id').unsigned().references('id').inTable('government_expenses').notNullable();
+		table.integer('government_expense_id').unsigned().references('id').inTable('government_expenses').notNullable();
 		table.integer('revenue_id').unsigned().references('id').inTable('revenues').notNullable();
 		table.integer('value').notNullable();
 		table.timestamp('date').notNullable().defaultTo(knex.fn.now());
