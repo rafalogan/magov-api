a83e8d8f7556849c3f1959ce5015ee24d9061af7
RafaDraw <712020:ad0a1fe8-71f7-4d68-bc04-c1239416cf45@bots.bitbucket.org>
2023-07-31 16:33:41 -0300

    remove plaintiff_id of tasks table

--- database/migrations/20230731192905_altertable_in_tasks.ts
+++ database/migrations/20230731192905_altertable_in_tasks.ts
@@ -1,10 +1,13 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+	return knex.schema.alterTable('tasks', (table: Knex.TableBuilder) => {
+		table.dropColumn('plaintiff_id');
+	});
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+	return knex.schema.alterTable('tasks', (table: Knex.TableBuilder) => {
+		table.integer('plaintiff_id').unsigned().references('id').inTable('plaintiffs').nullable();
+	});
 }
-
