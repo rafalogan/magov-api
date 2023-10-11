21b9af4dd38792a832bb49bcdd0c8cebbca62b38
Rafael Candeira <712020:ad0a1fe8-71f7-4d68-bc04-c1239416cf45@bots.bitbucket.org>
2023-10-11 14:21:20 -0300

    fix balances

--- src/repositories/models/government-revenue-view.model.ts
+++ src/repositories/models/government-revenue-view.model.ts
@@ -1,4 +1,4 @@
-import { convertBlobToString, convertToDate, setInstanceId } from 'src/utils';
+import { convertBlobToString, convertToDate, setInstanceId, setValueNumberToView } from 'src/utils';
 import { IGovernmentRevenueViewModel, IPropositionExpensesGovernment, IUnitGovernmentRevenue } from '../types';
 import isEmpty from 'is-empty';
 
@@ -17,12 +17,13 @@ export class GovernmentRevenueViewModel {
 	expenses: IPropositionExpensesGovernment[];
 	region: string;
 	tenancyId: number;
+	balance: number;
 
 	constructor(data: IGovernmentRevenueViewModel, id?: number) {
 		this.id = setInstanceId(id || data.id);
 		this.revenue = data.revenue.trim();
 		this.receive = convertToDate(data.receive);
-		this.value = data.value / 100;
+		this.value = setValueNumberToView(data.value);
 		this.status = Number(data.status);
 		this.active = !!data.active;
 		this.recurrent = !!data.recurrent;
@@ -33,6 +34,7 @@ export class GovernmentRevenueViewModel {
 		this.expenses = this.setPropositions(data.expenses);
 		this.region = data.region?.trim();
 		this.tenancyId = Number(data.tenancyId);
+		this.balance = setValueNumberToView(data.balance) || 0;
 	}
 
 	private setPropositions(data?: IPropositionExpensesGovernment[]): IPropositionExpensesGovernment[] {
