bd46e7509b10b2127702c41251ac2997559953f2
Rafael Candeira <712020:ad0a1fe8-71f7-4d68-bc04-c1239416cf45@bots.bitbucket.org>
2023-10-25 16:49:17 -0300

    fix types service

--- src/repositories/models/propositions-type.model.ts
+++ src/repositories/models/propositions-type.model.ts
@@ -5,6 +5,7 @@ import { IPropositionsType } from '../types';
 export class PropositionsTypeModel {
 	id?: number;
 	name: string;
+	fileId?: number;
 	description?: string;
 	active: boolean;
 	document?: FileEntity;
@@ -13,9 +14,10 @@ export class PropositionsTypeModel {
 	constructor(data: IPropositionsType, id?: number) {
 		this.id = setInstanceId(id || data.id);
 		this.name = data.name.trim();
+		this.fileId = Number(data.fileId);
 		this.description = convertBlobToString(data.description);
 		this.active = !!data.active;
-		this.document = data.document ? new FileEntity(data.document) : undefined;
+		this.document = data?.document ? new FileEntity(data?.document) : undefined;
 		this.parentId = data.parentId;
 	}
 }
