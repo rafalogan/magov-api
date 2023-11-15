cbc15501b39519a46aee250348dbe22ce5c79417
Rafael Candeira <712020:ad0a1fe8-71f7-4d68-bc04-c1239416cf45@bots.bitbucket.org>
2023-11-15 11:07:54 -0300

    update products table

---
+++ database/migrations/20231115134752_alter_tables_products.ts
@@ -0,0 +1,42 @@
+import { Knex } from 'knex';
+import { convertDataValues } from 'src/utils';
+
+export async function up(knex: Knex): Promise<void> {
+	const products = await knex('products');
+
+	await knex.schema.alterTable('products', (table: Knex.TableBuilder) => {
+		table.integer('type_id').unsigned().references('id').inTable('products_types').notNullable();
+	});
+
+	for (const { id, plan } of products) {
+		if (plan) {
+			const { id: type_id } = await knex('products_types').select('id').where({ type: 'plan' }).first();
+
+			await knex('products').update({ type_id }).where({ id });
+		}
+	}
+
+	return knex.schema.alterTable('products', (table: Knex.TableBuilder) => {
+		table.dropColumn('plan');
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	const products = await knex('products');
+
+	await knex.schema.alterTable('products', (table: Knex.TableBuilder) => {
+		table.boolean('plan').nullable();
+	});
+
+	const { id: type } = await knex('products_types').select('id').where({ type: 'plan' }).first();
+
+	for (const { id, type_id } of products) {
+		if (type_id === type) {
+			await knex('products').update({ plan: true }).where({ id });
+		}
+	}
+
+	return knex.schema.alterTable('products', (table: Knex.TableBuilder) => {
+		table.dropColumn('type_id');
+	});
+}
